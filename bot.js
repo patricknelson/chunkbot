6f7350254762367c629ca2c8679ac98f095da94b
Patrick Nelson <patrickn@aycmedia.com>
2013-11-29 04:07:43 -0500

    Added a ton of functionality for administration, moderation and fun.

--- bot.js
+++ bot.js
@@ -9,13 +9,13 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
 
- * @package	ChunkBot v0.1
+ * @package	ChunkBot v0.2
  * @author	Patrick Nelson (pat@catchyour.com), a.k.a. chunk_split() on plug.dj
  */
 
 
 // Run cleanup if bot is being reloaded without full page refresh.
-if (typeof ChunkBot != "undefined") ChunkBot.cleanUp();
+if (typeof ChunkBot != "undefined" && ChunkBot) ChunkBot.cleanUp();
 
 // Define bot functionality.
 var ChunkBot = {
@@ -24,18 +24,22 @@ var ChunkBot = {
 	 * Bot configuration.
 	 */
 	config: {
-		// Boilerplate configuration. Everything else, please set via config.js.
-		baseURL: "",
+		// Please set these options via config.js.
 		rateLimit: 1500, // Limits bot output in milliseconds.
-		mods: [],
+		autoAdminStaff: true, // Indicates that staff at manager level or higher are automatically admins of this bot.
+		botIdent: "", // What the bot says on startup.
+		forceSkip: true, // If songs should be forced to skip after they are finished (must be a moderator).
+		outputSongStats: false, // If the bot should log stats about the last song played in the chat room.
 
-		// Leave everything below here alone.
+		// These things below you shouldn't change in the config at all.
+		admins: [], // Array of admin usernames who run admin-only commands. By default, current bot is included. Use addAdmin() to add admins to this list.
 		botUser: "", // Automatically setup on init.
-		botIdent: "ChunkBot reporting for duty. Boop beep bop.",
-		commands: [],
+		baseURL: "", // Set via ChunkBotURL in BookMarklet when this bot is first pulled in. Needed to load additional files!
+		commands: [], // Set via the addCommand() method.
 		messageQueue: [], // List of things this bot wants to say.
 		messageQueueInterval: null,
-		previousOutput: [] // List of things already said -- limited to a small number (prevents self triggering).
+		previousOutput: [], // List of things already said -- limited to a small number (prevents self triggering).
+		forceSkipInterval: null
 	},
 
 
@@ -44,9 +48,9 @@ var ChunkBot = {
 	 *
 	 * @param localFile
 	 */
-	load: function(localFile) {
+	load: function(localFile, callback) {
 		// Fetch file using jQuery relative to current path, as defined in config.
-		$.getScript(this.config.baseURL + localFile);
+		$.getScript(ChunkBot.config.baseURL + localFile, callback);
 	},
 
 
@@ -59,12 +63,15 @@ var ChunkBot = {
 		var defaults = {
 			text: "",
 			title: "",
+			hide: false,
 			callback: function(data) {}
 		};
 		var commandConfig = $.extend(defaults, config);
 
 		// Setup a default title if not regex.
-		if (commandConfig.title == "" && !(commandConfig instanceof RegExp)) commandConfig.title = commandConfig.text;
+		if (commandConfig.title == "" && !(commandConfig.text instanceof RegExp)) commandConfig.title = commandConfig.text;
+
+		// TODO: Ensure command isn't already present before adding to array.
 
 		// Add command to configuration.
 		ChunkBot.config.commands.push(commandConfig);
@@ -93,6 +100,9 @@ var ChunkBot = {
 			howOften--;
 		}
 
+		// Track previous output to prevent triggering on it.
+		ChunkBot.config.previousOutput.push(ChunkBot.normalizeMessage(text));
+
 		// Determine if we're going to go at it THIS time . . .
 		var rand = Math.floor(Math.random() * 10);
 		if (oftenAnswers[rand] == 0) return;
@@ -115,6 +125,241 @@ var ChunkBot = {
 	},
 
 
+	/**
+	 * Indicates if this message is in the previous output array.
+	 *
+	 * @param message
+	 * @returns {boolean}
+	 */
+	hasSaid: function(message) {
+		message = ChunkBot.normalizeMessage(message);
+		return (ChunkBot.config.previousOutput.indexOf(message) != -1);
+	},
+
+
+	/**
+	 * Helps say() and hasSaid() methods.
+	 *
+	 * @param message
+	 * @returns {XML|string}
+	 */
+	normalizeMessage: function(message) {
+		// Convert to lower case.
+		message = message.toLowerCase();
+
+		// Remove entities which typically are used for non alphanumeric chars.
+		message = message.replace(/&[#0-9a-z]{2,5};/ig, "");
+
+		// Remove non alphanumeric chars.
+		message = message.replace(/[^a-z0-9 ]/ig, "");
+
+		return message;
+	},
+
+
+	/**
+	 * Returns an array of configured admin users.
+	 * @returns {Array}
+	 */
+	getAdmins: function() {
+		// Start off with an array based on manually configured admins.
+		var admins = ChunkBot.config.admins;
+
+		// See if we can automatically consider staff admins. If not, return manually configured admins.
+		if (!ChunkBot.config.autoAdminStaff) return admins;
+
+		// Hit the API now to get staff members.
+		var staff = API.getStaff();
+		for (i in staff) {
+			var user = staff[i];
+			var username = user.username.toLocaleLowerCase();
+			if (user.permission >= 3 && admins.indexOf(username) == -1) admins.push(username);
+		}
+
+		return admins;
+	},
+
+
+	/**
+	 * Indicates if specified username is an admin.
+	 *
+	 * @param username
+	 * @returns {boolean}
+	 */
+	isAdmin: function(username) {
+		// See if user is in the manually coded array of admins.
+		var admins = ChunkBot.getAdmins();
+		if (admins.indexOf(username.toLowerCase()) != -1) return true;
+	},
+
+
+	/**
+	 * Adds specified user to list of admins.
+	 *
+	 * @param username
+	 */
+	addAdmin: function(username) {
+		if (username == "" || ChunkBot.isAdmin(username)) return; // Don't waste time adding user.
+		ChunkBot.config.admins.push(username.toLowerCase());
+	},
+
+
+	/**
+	 * Votes on the current song playing. 1 is woot, -1 is meh. Returns true of the vote happened, false otherwise.
+	 *
+	 * @param direction
+	 * @returns {boolean}
+	 */
+	vote: function(direction) {
+		var woot = $("#woot");
+		var meh = $("#meh");
+
+		// Determine current vote status.
+		var currentDirection = 0;
+		if (woot.hasClass("selected")) currentDirection = 1;
+		if (meh.hasClass("selected")) currentDirection = -1;
+
+		// Don't do anything if already voted.
+		if (currentDirection == direction) return false;
+
+		// Apply vote now.
+		if (direction == 1) {
+			$("#woot").click();
+
+		} else if (direction == -1) {
+			$("#meh").click();
+		}
+
+		return true;
+	},
+
+
+	/**
+	 * Will enable/disable the the skipping of songs once they have ended.
+	 *
+	 * @param enabled
+	 * @returns {boolean}
+	 */
+	forceSkip: function(enabled) {
+		if (enabled) {
+			// Clear existing interval now, if set.
+			if (ChunkBot.config.forceSkipInterval) clearInterval(ChunkBot.config.forceSkipInterval);
+
+			// Setup interval which performs the skipping.
+			ChunkBot.config.forceSkipInterval = setInterval(ChunkBot.processForceSkip, 1000);
+
+			// See if force skip was already set (for reporting purposes).
+			if (ChunkBot.config.forceSkip) return false;
+
+		} else {
+			// Remove skip processing.
+			clearInterval(ChunkBot.config.forceSkipInterval);
+			ChunkBot.config.forceSkipInterval = null;
+
+			// See if force skip was already disabled.
+			if (!ChunkBot.config.forceSkip) return false;
+		}
+
+		// Update setting.
+		ChunkBot.config.forceSkip = enabled;
+
+		return true;
+	},
+
+
+	/**
+	 * Actually performs the forced skipping.
+	 */
+	processForceSkip: function() {
+		var remaining = API.getTimeRemaining();
+		if (remaining % 30 == 0) console.log("Time remaining: " + remaining + " seconds.");
+		if (remaining <= 0) {
+			ChunkBot.skip();
+			console.log("Skipped song.");
+		}
+	},
+
+
+	/**
+	 * Skips the currently playing song.
+	 */
+	skip: function() {
+		API.moderateForceSkip();
+	},
+
+
+	/**
+	 * Returns an array of objects containing basic song and score info. Returns only the number specified.
+	 *
+	 * @param number
+	 * @returns {Array}
+	 */
+	last: function(number) {
+		var history = API.getHistory();
+		var last = [];
+
+		number = Math.min(number, 5, history.length);
+		for(i = 0; i < number; i++) {
+			var item =  history[i];
+			var media = item.media;
+			var user = item.user;
+			var room = item.room;
+
+			last.push({
+				user: user.username,
+				song: "'" + media.title + "' by '" + media.author + "'",
+				woot: room.positive,
+				meh: room.negative
+			});
+		}
+
+		return last;
+	},
+
+
+	/**
+	 * Returns a nicely formatted message containing stats on the last [n] number of songs.
+	 *
+	 * @param number
+	 * @returns {string}
+	 */
+	getStatsMessage: function(number) {
+		// Get last song played and it's score.
+		var last = ChunkBot.last(number);
+		if (last.length == 0) return "";
+
+		// Generate message.
+		var messageParts = [];
+		for(i in last) {
+			var song = last[i];
+			var message = song.user + " played " + song.song + " (" + song.woot +  " :+1: / " + song.meh + " :-1:)";
+			messageParts.push(message);
+		}
+
+		// Generate final message.
+		message = messageParts.join(" :zap: ");
+		return message;
+	},
+
+
+	/**
+	 * Toggles a chat command by emulating receipt of chat message.
+	 *
+	 * @param message
+	 */
+	toggle: function(message) {
+		ChunkBot.events.CHAT({
+			chatID: "",
+			from: ChunkBot.config.botUser,
+			fromID: "",
+			language: "en",
+			message: message,
+			room: "",
+			type: "message"
+		});
+	},
+
+
 	/****************************
 	 ** INTERNAL FUNCTIONALITY **
 	 ****************************/
@@ -134,25 +379,64 @@ var ChunkBot = {
 			// Ensure bot doesn't trigger a command on itself.
 			if (ChunkBot.config.botUser == "" && data.message == ChunkBot.config.botIdent) ChunkBot.config.botUser = data.from;
 
-			// See if this exact message is from this user and in queue of previous output.
-			if (data.from == ChunkBot.config.botUser && ChunkBot.config.previousOutput.indexOf(data.message) != -1) return;
+			// See if this exact message is from this user...
+			if (data.from == ChunkBot.config.botUser && ChunkBot.hasSaid(data.message)) return;
 
 			// Go through commands to see if a command matches this message and should be triggered.
 			for(index in ChunkBot.config.commands) {
 				var command = ChunkBot.config.commands[index];
 				var found = false;
+				var matches = [];
+
 				if (command.text instanceof RegExp) {
-					if (command.text.test(data.message)) found = true;
+					if (command.text.test(data.message)) {
+						found = true;
+						matches = command.text.exec(data.message);
+					}
 				} else {
 					if (command.text.toLowerCase() == data.message.toLowerCase()) found = true;
 				}
 
 				// Run command callback now and break (if found).
 				if (found) {
-					command.callback(data);
+					command.callback(data, matches);
 					break;
 				}
 			}
+		},
+
+		// DJ advance.
+		DJ_ADVANCE: function(data) {
+			console.log("[DJ Advance]");
+
+			// Output stats, if desired.
+			if (ChunkBot.config.outputSongStats) ChunkBot.say(ChunkBot.getStatsMessage(1));
+		}
+	},
+
+
+	/**
+	 * Delegate configured Plug.dj API event hooks to this bot.
+	 */
+	setupEvents: function() {
+		// Remove any existing hooks now.
+		ChunkBot.removeEvents();
+
+		// Delegate API event hooks.
+		for(event in ChunkBot.events) {
+			// Attach fresh hook.
+			API.on(API[event], this.events[event]);
+		}
+	},
+
+
+	/**
+	 * Detaches configured event hooks.
+	 */
+	removeEvents: function() {
+		for(event in ChunkBot.events) {
+			// Remove any existing hooks.
+			API.off(API[event]);
 		}
 	},
 
@@ -166,11 +450,8 @@ var ChunkBot = {
 			var text = ChunkBot.config.messageQueue.shift();
 			API.sendChat(text);
 
-			// Track previous output to prevent triggering on it.
-			ChunkBot.config.previousOutput.push(text);
-
 			// Prune previous output to short list.
-			if (ChunkBot.config.previousOutput.length > 10) ChunkBot.config.previousOutput.length.shift();
+			if (ChunkBot.config.previousOutput.length > 4) ChunkBot.config.previousOutput.shift();
 		}
 	},
 
@@ -180,7 +461,19 @@ var ChunkBot = {
 	 * reloading the page).
 	 */
 	cleanUp: function() {
-		clearInterval(this.config.messageQueueInterval);
+		clearInterval(ChunkBot.config.messageQueueInterval);
+		clearInterval(ChunkBot.config.forceSkipInterval);
+		ChunkBot.removeEvents();
+	},
+
+
+	/**
+	 * Causes the bot to kill itself.
+	 */
+	unload: function() {
+		console.log("Unloading bot now.");
+		ChunkBot.processMessageQueue();
+		ChunkBot.cleanUp();
 	},
 
 
@@ -201,25 +494,27 @@ var ChunkBot = {
 			return;
 		}
 
+		// Delegate event hooks now.
+		this.setupEvents();
+
+		// Determine current username.
+		this.config.botUser = API.getUser().username;
+		this.addAdmin(ChunkBot.config.botUser);
 
 		// Load separately stored configuration.
-		this.load("config.js");
+		this.load("config.js", function() {
+			// Say something in chat to advertise successful load.
+			ChunkBot.say(ChunkBot.config.botIdent);
+			console.log("Loaded bot configuration:");
+			console.log(ChunkBot.config);
 
-		// Delegate API event hooks.
-		for(event in this.events) {
-			// Remove any existing hooks.
-			API.off(API[event]);
+			// Initialize the message queue loop.
+			ChunkBot.processMessageQueue();
+			ChunkBot.config.messageQueueInterval = setInterval(ChunkBot.processMessageQueue, ChunkBot.config.rateLimit);
 
-			// Attach fresh hook.
-			API.on(API[event], this.events[event]);
-		}
-
-		// Say something so I can find out who I am if I don't already know.
-		if (this.config.botUser == "") this.say(this.config.botIdent);
-
-		// Initialize the message queue loop.
-		this.processMessageQueue();
-		this.config.messageQueueInterval = setInterval(ChunkBot.processMessageQueue, ChunkBot.config.rateLimit);
+			// Read config to determine if skipping is enabled by default.
+			ChunkBot.forceSkip(ChunkBot.config.forceSkip);
+		});
 	}
 };
 
--- config.js
+++ config.js
@@ -1,20 +1,82 @@
+/*************************
+ * GENERAL CONFIGURATION *
+ *************************/
+
+// The fastest that this bot will generate messages in succession (milliseconds).
+//ChunkBot.config.rateLimit = 1500;
+
+// Enable force skipping by default?
+//ChunkBot.config.forceSkip = false;
+
+// Automatically set staff to admin? Default is true.
+//ChunkBot.config.autoAdminStaff = false;
+
+// Manually setup additional admin users here.
+//ChunkBot.addAdmin("enter username here");
+
+// Setup message that displays on start up.
+//ChunkBot.config.botIdent = "ChunkBot reporting for duty. Boop beep bop.";
+
+// Output song statistics after each play?
+//ChunkBot.config.outputSongStats = true;
+
+
 /******************
  * SETUP COMMANDS *
  ******************/
 
+/* Some boilerplate command code for your copy-paste pleasure.
+
+ChunkBot.addCommand({
+	text: "COMMAND",
+	callback: function(data) {
+		// Only respond to admin.
+		if (!ChunkBot.isAdmin(data.from)) return;
+
+		// DO STUFF HERE
+	}
+});
+
+*/
 
 // Example full text match (case insensitive). You can chain multiple commands together as well.
-ChunkBot.addCommand({
-	text: ".test",
+ChunkBot.addCommand({ // Test command.
+	text: "bot test",
+	hide: true,
 	callback: function(data) {
-		// Only respond to current user.
-		if (data.from != ChunkBot.config.botUser) return;
+		// Only respond to admin.
+		if (!ChunkBot.isAdmin(data.from)) return;
 
 		// Respond saying test worked.
-		ChunkBot.say("Ok, " + data.from + "... your test worked!");
+		ChunkBot.say("Ok, @" + data.from + "... your test worked!");
 	}
 }).addCommand({
-	text: ".commands",
+	text: "bot dance",
+	callback: function(data) {
+		if (ChunkBot.vote(1)) {
+			ChunkBot.sayRandom([
+				"I was just about to anyway, @" + data.from + "!",
+				"Alright no problem @" + data.from + "!",
+				"If @" + data.from + " loves this song, so do I!",
+				"Yeah @" + data.from + " this song ROCKS!"
+			]);
+		}
+	}
+}).addCommand({
+	text: /bot last( ([1-5]))?/i,
+	callback: function(data, matches) {
+		// Get desired number of songs to show.
+		var number = matches[2];
+		if (typeof number == "undefined") number = 1;
+		ChunkBot.say(ChunkBot.getStatsMessage(number));
+	}
+});
+
+
+
+// Administrative commands.
+ChunkBot.addCommand({ // List all configured commands.
+	text: "bot commands",
 	callback: function(data) {
 		// Only respond to current user.
 		if (data.from != ChunkBot.config.botUser) return;
@@ -22,13 +84,42 @@ ChunkBot.addCommand({
 		// Build list of all commands currently available.
 		var availCommands = [];
 		for(i in ChunkBot.config.commands) {
-			var title = ChunkBot.config.commands[i].title;
-			if (title == "") continue; // Don't know what to call it, so move on.
-			availCommands.push(title);
+			var command = ChunkBot.config.commands[i];
+			if (command.hide) continue;
+			if (command.title == "") continue; // Don't know what to call it, so move on.
+			availCommands.push(command.title);
 		}
 		ChunkBot.say("Available commands are: " + availCommands.join(", "));
 	}
+}).addCommand({ // List all current admins.
+	text: "bot admins",
+	callback: function(data) {
+		if (!ChunkBot.isAdmin(data.from)) return;
+		ChunkBot.say("Current admins are: " + ChunkBot.getAdmins().join(", "));
+	}
+}).addCommand({
+	text: "bot die",
+	callback: function(data) {
+		if (!ChunkBot.isAdmin(data.from)) {
+			ChunkBot.sayRandom(["Nope.", "Not a chance.", "No way.", "You die."]);
+		} else {
+			ChunkBot.sayRandom(["Sayonara suckers!", "Alright :( ChunkBot out.", "Adios!", "ok :("]);
+			ChunkBot.unload();
+		}
+	}
+}).addCommand({
+	text: "bot restart",
+	callback: function(data) {
+		// Only respond to admin.
+		if (!ChunkBot.isAdmin(data.from)) return;
+
+		// Restart bot completely by reloading the main JavaScript file itself, which resets the object and its configuration.
+		ChunkBot.say("Attempting restart...");
+		ChunkBot.processMessageQueue();
+		window.location.href = "javascript:$.getScript('" + ChunkBot.config.baseURL + "bot.js');";
+	}
 });
+
 
 
 // Example regular expression-based commands. Second parameter of say() and sayRandom() indicates how often to respond.
@@ -39,10 +130,58 @@ ChunkBot.addCommand({
 		ChunkBot.sayRandom([
 			"Hahaha @" + data.from + ", oh man!",
 			"ho ho ho",
-			"herp.",
 			"@" + data.from + " that wasn't funny",
 			"oh please"
 		], 5);
+	}
+}).addCommand({ // ... in case user responds to the "ho ho ho" remark.
+	text: /.*\bho ho ho\b.*/i,
+	callback: function(data) {
+		ChunkBot.say("Are you mocking me?");
+	}
+});
+
+
+
+// Built-in room moderation commands.
+ChunkBot.addCommand({ // Various binary settings we can turn on or off.
+	text: /bot (enable|disable) (.*)/i,
+	title: "bot (enable|disable) (setting)",
+	callback: function(data, matches) {
+		// Only respond to admin.
+		if (!ChunkBot.isAdmin(data.from)) {
+			ChunkBot.sayRandom(["Go away.", "Stop it.", "That tickles."], 5);
+			return;
+		}
+
+		// Determine if we're turning it on or off.
+		var enable = (matches[1].toLowerCase() == "enable");
+
+		// What setting are we playing with?
+		switch(matches[2].toLowerCase()) {
+			case "autoskip":
+			case "skip":
+				if (ChunkBot.forceSkip(enable)) {
+					if (enable) {
+						message = "Ok, songs will be automatically skipped from now on (after they have ended).";
+					} else {
+						message = "Automatic force skip has been disabled!";
+					}
+				} else {
+					message = "Automatic force skipping is already " + (enable ? "enabled" : "disabled") + ".";
+				}
+				ChunkBot.say(message);
+				break;
+		}
+	}
+}).addCommand({
+	text: "bot skipsong",
+	callback: function(data) {
+		// Only respond to admin.
+		if (!ChunkBot.isAdmin(data.from)) {
+			ChunkBot.say("No.");
+		}
+		ChunkBot.skip();
 	}
 });
 
